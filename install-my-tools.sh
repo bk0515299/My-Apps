d4031a3006d59f49d8cbb2773fbb793b7a5f62d8
Winston <bk0515299@gmail.com>
2025-08-03 20:38:01 -0400

    Update install-my-tools.sh

--- install-my-tools.sh
+++ install-my-tools.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo wget https://github.com/bk0515299/My-Apps/raw/refs/heads/main/my-tools-1.0-1.fc42.noarch.rpm | echo "installing 1/2"
+sudo wget https://github.com/bk0515299/My-Apps/releases/download/favtools-v1.0.0/my-tools-1.0-1.fc42.noarch.rpm | echo "installing 1/2"
 sudo dnf install my-tools-1.0-1.fc42.noarch.rpm | echo "installing 2/2"
+sudo rm -rf https://github.com/bk0515299/My-Apps/releases/download/favtools-v1.0.0/my-tools-1.0-1.fc42.noarch.rpm | echo "Cleaning up..."
